da0885ced616f3c47c8e03b012f2e1671eddf2ff
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2025-07-07 06:53:07 -0300

    Background color changed to brightnnes

--- frontend/src/components/SideBar/CreditsBox.jsx
+++ frontend/src/components/SideBar/CreditsBox.jsx
@@ -4,7 +4,7 @@ import { GreenLinkButton } from "../UI/links/GreenLinkButton";
 export function CreditsBox(){
    const credits = 1000;
    return(
-      <section className="flex flex-col gap-1 items-center mt-5 bg-[var(--credits-box-background)] brightness-95 h-fit w-50 p-2 py-3 rounded-md">
+      <section className="flex flex-col gap-1 items-center mt-5 bg-[var(--background)] brightness-90 h-fit w-50 p-2 py-3 rounded-md">
          <p className="text-sm">Créditos diponíveis</p>
          <span className="font-bold text-xl">{new Intl.NumberFormat().format(credits)}</span>
          <GreenLinkButton href={'/credits'} text={'Comprar Créditos'} />
