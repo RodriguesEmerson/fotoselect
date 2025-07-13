dcd691287305b1c7cc87c841f608f6a50dea6d37
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2025-07-13 04:41:37 -0300

    Headers Updated

--- backend/index.php
+++ backend/index.php
@@ -1,17 +1,16 @@
 <?php 
 header('Content-Type: application/json; charset=UTF-8');
-header("Access-Control-Allow-Origin: http://localhost:3000");  // Permitir apenas localhost:3000
+header("Access-Control-Allow-Origin: http://localhost:3000");  // Allows only localhost:3000
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, origin");
-header("Access-Control-Allow-Credentials: true"); //Obrigatório o envio de: credentials: 'include' no header pelo front.
+header("Access-Control-Allow-Headers: Content-Type, Authorization, Origin");
+header("Access-Control-Allow-Credentials: true"); //Allows frontend to send cookies.
 // header("Access-Control-Max-Age: 3600");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
    http_response_code(200);
    exit();
 }
-$request =str_replace( '/organizze-bk/public/', '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-$method = $_SERVER['REQUEST_METHOD'];
+
 //Enable namespaces
 require_once __DIR__ . '/vendor/autoload.php';
 
