21a035489c672888dff9a99fc96448fa5cdefe3b
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2025-07-22 02:16:15 -0300

    2207 | Now the click event is passed by props

--- frontend/src/components/modals/ModalBackground.jsx
+++ frontend/src/components/modals/ModalBackground.jsx
@@ -1,12 +1,9 @@
-import { useModalVisibility } from "@/Zustand/useModalVisibility";
 
-
-export function ModalBackground({ children }){
-   const setIsNewGalleryModalVisible = useModalVisibility(state => state.setIsNewGalleryModalVisible);
+export function ModalBackground({ children, onMouseDown }){
    return(
       <div 
          className={`fixed flex justify-center items-center z-10 left-0 top-0 h-[100vh] w-[100vw] bg-[#00000050]`}
-         onMouseDown={() => setIsNewGalleryModalVisible(false)}
+         onMouseDown={onMouseDown()}
       >
          {children}
       </div>
