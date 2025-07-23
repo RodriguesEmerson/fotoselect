48eea2e169aefc0f852b59161159a4756ba7c9d0
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2025-07-22 23:17:07 -0300

    2207 | Updated

--- frontend/src/Zustand/useModalVisibility.js
+++ frontend/src/Zustand/useModalVisibility.js
@@ -2,7 +2,11 @@ import { create } from "zustand";
 
 export const useModalVisibility = create(set => ({
    isNewGalleryModalVisible: false,
+   isConfirmDecisionModalVisible: false,
    setIsNewGalleryModalVisible: (visibility) => set(() => ({
       isNewGalleryModalVisible: visibility
+   })),
+   setIsConfirmDecisionModalVisible: (visibility) => set(() => ({
+      isConfirmDecisionModalVisible: visibility
    }))
 }))
